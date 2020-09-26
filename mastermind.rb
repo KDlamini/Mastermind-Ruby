066bc334f86186712cfe1fc3673de51b36024b08
Simo Nkosi <wilzmar.kwd@gmail.com>
2020-09-27 01:10:04 +0200

    Created a working Codebreaker class, added flags and handled wins

--- mastermind.rb
+++ mastermind.rb
@@ -2,12 +2,16 @@ require_relative 'colors'
 
 #Initiate Game
  class Game
-    attr_accessor :board
-    @@colors = {"r" => :red, "g" => :green, "b" => :blue, "w" => :white, "y" => :yellow } 
+    attr_accessor :board, :rows, :columns
+    attr_reader :secret_code
+    @@colors = {"r" => :red, "g" => :green, "b" => :blue, "w" => :white, "y" => :yellow }
+    
 
     def initialize()
-        @board = Board.new(10,4)
-        play_game()
+        @columns = 4
+        @rows = 10
+        @board = Board.new(@rows, @columns)
+        @secret_code = computer_generated_code()
     end
 
     def play_game
@@ -15,12 +19,25 @@ require_relative 'colors'
         set_up()
     end
 
+    #Color selection for player to choose combinations
+    def color_options
+        print "   _______________________________________________\n\n"
+        @@colors.each do |(k,v)|
+            print "  | #{k} = #{Token.new.new_token[:token] = "\u25cf".send(v)} "
+        end
+        print "|\n   _______________________________________________\n"
+        print "Select the correct letter matching a respective color. \n"
+        print "Create a combination of #{@columns} colors without spaces or commas.\n\n"
+    end
+
     protected
 
+    #Welcome message
     def welcome
         print "------------Welcome to Mastermind-----------\n"
     end
 
+    #Layout for choosing to either make code or break code
     def set_up
         print "To be a Codemaker, Press 0 \n"
         print "To be a Codebreaker, Press 1 \n"
@@ -33,31 +50,31 @@ require_relative 'colors'
             print "------------Welcome Codebreaker------------- \n"
             color_options()
             @board.display_board()
+            Codebreaker.new(@@colors)
         else
             "Please select 0 or 1"
             set_up()
         end
     end
 
-    def color_options
-        print "_____________________________________________\n\n"
-        @@colors.each do |(k,v)|
-            print "| #{k} = #{Token.new.new_token[:token].send(v)} "
-        end
-        print "|\n_____________________________________________\n"
-        print "Select the correct letter matching a respective color. \n"
-        print "Create a combination of 4 colors without spaces or commas. \n"
+    #Randomly generate secret code when game is initiated
+    def computer_generated_code
+        code = ""
+        keys = ["r","g","b","w","y"]
+        @columns.times { code.concat(keys[rand(5)]) }
+        code
     end
 end
 
 #Initiate a Board class tha will populate board
 class Board
-    attr_accessor :board
+    attr_accessor :new_board
 
     def initialize(row_number, column_number)
-        @board = create_board(row_number, column_number)
+        @new_board = create_board(row_number, column_number)
     end
 
+    #Use provided rows and columns to populate board
     def create_board(row_number, column_number)
         board = Array.new
         row = Array.new
@@ -70,18 +87,19 @@ class Board
         board
     end
 
+    #Display board with default tokens
     def display_board
-        board.each do |row|
-            print "| "
+        @new_board.each do |row|
+            print "                 | "
             row.each do|obj|
-                print "#{obj.new_token[:token].send(obj.new_token[:color])} "
+                print "#{obj.new_token[:token].send(obj.new_token[:color])}  "
             end
-            print " |\n"
+            print "\n"
         end
     end
 end
 
-#Create tokens that will populate on the board
+#Create token class that will populate tokens on the board
 class Token
     attr_accessor :new_token
     def initialize(token = "\u25cb", color = :black)
@@ -91,9 +109,123 @@ end
 
 #Create player that will attempt to break code
 class Player
-    def initialize()
+    attr_accessor :board, :player_code
+    attr_reader :columns, :colors
+
+    def initialize(colors, board, columns)
+        @colors = colors
+        @columns = columns
+        @board = board
+        @player_code = ""
+    end
+
+    #Retrieve player combinations and validate
+    def user_input
+        guesses = Array.new
+        begin
+            input = gets.chomp
+            raise if !input_valid?(input)
+        rescue StandardError
+            print "Please enter the correct combination of #{@columns} letters:\n"
+            retry
+        end
         
+        @player_code = input
+        code = input.split('')
+        
+        code.each do |key|
+            guesses.push(Token.new("\u25cf", @colors[input[key]]))
+        end
+        guesses
+    end
+
+    protected
+    
+    #Create a method that will validate user input
+    def input_valid?(input)
+        control = [true] * @columns
+        test_arr = Array.new
+        code = input.split("")
+        code.each do |key|
+            if @colors.has_key?(key)
+                test_arr.push(true)
+            end
+        end
+
+        test_arr == control ? true : false
+    end
+end
+
+#Initiate class that will handle guesses fron player
+class Codebreaker
+    def initialize(colors)
+        @colors = colors
+        @columns = Game.new.columns
+        @rows = Game.new.rows
+        @board = Game.new.board
+        @secret_code = Game.new.secret_code
+        initiate_code_break()
+    end
+
+    def initiate_code_break
+        player = Player.new(@colors, @board, @columns)
+        i = 0
+
+        @rows.times do
+            #puts @secret_code
+            user_input = player.user_input()
+            player_code = player.player_code
+
+            if player_code == @secret_code
+                @board.new_board[i] = user_input + flags(player_code, @secret_code)
+                Game.new.color_options()
+                @board.display_board()
+                print "\n      ------------Congratulations!!!-----------"
+                print "\n      You have succesfully broken the code! wow!\n"
+                break
+            end
+            
+            @board.new_board[i] = user_input + flags(player_code, @secret_code)
+            Game.new.color_options()
+            @board.display_board()
+
+            i += 1
+        end
+
+        if player.player_code != @secret_code
+            print "\n       You have failed to break the code!"
+            print "\n       -----------Game Over!!!-----------\n" 
+        end
+    end
+
+    protected
+
+    #Create flags that will give hints about secret code combination
+    def flags(player_code, secret_code)
+        guess = player_code.slice(0..player_code.size)
+        computer_code = secret_code.slice(0..secret_code.size)
+        looper = computer_code.size
+        flags = Array.new
+        i = 0; j = 0
+
+        looper.times do
+            if computer_code[i] == guess[i]
+                flags.push(Token.new("\u2691", :red))
+                guess[i] = "O"; computer_code[i] = "X"
+            end
+            i += 1
+         end
+         
+        looper.times do
+            if computer_code.include?(guess[j])
+                flags.push(Token.new("\u2691", :white))
+                computer_code[computer_code.index(guess[j])] = "X"  
+            end
+            j += 1
+        end
+        flags
     end
 end
 
 mastermind = Game.new
+mastermind.play_game()
